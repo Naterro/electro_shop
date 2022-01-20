fb0bfc805a4b1ed0b0bfe26e2a3fa82800167993
Naterro <nerterro@gmail.com>
2022-01-20 22:45:32 +0600

    Dried category page

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,10 +1,5 @@
 import React, {useEffect} from 'react';
-import {useTypedSelector} from "../hooks/useTypedSelector";
-import {useDispatch} from "react-redux";
 import {useParams} from "react-router-dom";
-import {fetchDevices} from "../store/action-creators/device";
-import {DEVICE_ROUTE} from "../utils/consts";
-import {Image} from "react-bootstrap";
 import DeviceList from "../components/DeviceList";
 
 const Category = () => {
