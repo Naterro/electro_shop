192525f5c62d84960c307a2a9a439596778472bf
Naterro <nerterro@gmail.com>
2022-01-20 22:45:17 +0600

    Dried category page

--- src/components/DeviceList.tsx
+++ src/components/DeviceList.tsx
@@ -6,7 +6,6 @@ import {DEVICE_ROUTE} from "../utils/consts";
 import {Image} from "react-bootstrap";
 
 const DeviceList : React.FC = ({children, ...props}) => {
-    //const state = useTypedSelector(state=> state.device)
     const {devices,error,loading} = useTypedSelector(state=> state.devices)
     const dispatch = useDispatch();
     useEffect(()=>{
@@ -32,20 +31,36 @@ const DeviceList : React.FC = ({children, ...props}) => {
     return (
         <div className="main-list">
             {devices.map(device=> {
-               return <div key={device.id} className="main-item">
-                   <a href={DEVICE_ROUTE + '/' + device.id}>
-                       <div className="item-grid">
-                           <div>
-                       <Image width={300} height={300}  />
-                           </div>
-                           <div className="item-text">
-                           <h4>{device.name}</h4>
-                           <p>{`Цена: ${device.price} У.Е.`}</p>
-                           </div>
-                       </div>
-                       </a>
-
-                   </div>
+                if(children)if(device.category==children) {
+                    return <div key={device.id} className="main-item">
+                        <a href={DEVICE_ROUTE + '/' + device.id}>
+                            <div className="item-grid">
+                                <div>
+                                    <Image width={300} height={300}/>
+                                </div>
+                                <div className="item-text">
+                                    <h4>{device.name}</h4>
+                                    <p>{`Цена: ${device.price} У.Е.`}</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                }
+                if (!children) {
+                    return <div key={device.id} className="main-item">
+                        <a href={DEVICE_ROUTE + '/' + device.id}>
+                            <div className="item-grid">
+                                <div>
+                                    <Image width={300} height={300}/>
+                                </div>
+                                <div className="item-text">
+                                    <h4>{device.name}</h4>
+                                    <p>{`Цена: ${device.price} У.Е.`}</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                }
             })}
         </div>
     );
--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -5,42 +5,16 @@ import {useParams} from "react-router-dom";
 import {fetchDevices} from "../store/action-creators/device";
 import {DEVICE_ROUTE} from "../utils/consts";
 import {Image} from "react-bootstrap";
+import DeviceList from "../components/DeviceList";
 
 const Category = () => {
     const {category} = useParams();
-    const {devices,error,loading} = useTypedSelector(state=> state.devices)
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(fetchDevices());
-    }, [dispatch])
-    if (loading){
-        return <h1>Loading</h1>
-    }
-    if (error){
-        return <h1>{error}</h1>
-    }
-    console.log(category);
-
     return (
-        <div className="main-list">
-            {devices.map(device=> {
-                if(device.category==category){
-                return <div key={device.id} className="main-item">
-                    <a href={DEVICE_ROUTE + '/' + device.id}>
-                        <div className="item-grid">
-                            <div>
-                                <Image width={300} height={300}  />
-                            </div>
-                            <div className="item-text">
-                                <h4>{device.name}</h4>
-                                <p>{`Цена: ${device.price} У.Е.`}</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            }})}
+        <div>
+            <DeviceList children={category}/>
         </div>
     );
+
 };
 
 export default Category;
